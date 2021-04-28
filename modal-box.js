c18a1c3cdf0033dcabd663848b1805bb32909104
Nguyen Quang Duy <s3877991@rmit.edu.vn>
2021-04-28 16:21:38 +0700

    JavaScript Task #3 COMPLETE. (Fully Responsive)

---
+++ modal-box.js
@@ -0,0 +1,35 @@
+/* Original JS code sourced and adapted for educational purposes:
+https://www.w3schools.com/howto/howto_css_cards.asp */
+
+// Activate the cards to trigger modal boxes
+let cards = document.getElementsByClassName("card");
+
+// Acivate the modal boxes
+let bios = document.getElementsByClassName("bio");
+
+// Activate the close button
+let closes = document.getElementsByClassName("close");
+
+// There are 4 member cards which display 4 modal boxes with their bios
+// Because these cards and modal boxes do the same behaviour, Duy will set a loop to make code cleaner and more efficient
+for (let index = 0; index < 4; index++) {
+
+    // Display modal box
+    cards[index].onclick = function () {
+        bios[index].style.display = "block";
+    }
+
+    // Close modal box
+    // If user click on close button
+    closes[index].onclick = function () {
+        bios[index].style.display = "none";
+    }
+
+    // If user click anywhere outside the modal
+    window.onclick = function (event) {
+        if (event.target == bios[index]) {
+            bios[index].style.display = "none";
+        }
+    }
+
+}
