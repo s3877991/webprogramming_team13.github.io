c726d3fac6b13971142c84ba44beecdff1cfe961
Nguyen Quang Duy <s3877991@rmit.edu.vn>
2021-05-08 10:58:58 +0700

    Trang's work Modified: Redirect changed #1

--- eworld-website/javascript/add-to-cart.js
+++ eworld-website/javascript/add-to-cart.js
@@ -1,31 +1,36 @@
 // Assigment 2: JAVASCRIPT ASSIGNMENT - TASK #7: Add Items To Cart
 
 // Get product information
-var addToCartButtons = document.getElementsByClassName('add-to-cart')
-var title = document.querySelector('#title').innerText
-var picture = document.getElementById('item-image').src
-var price = document.querySelector('.add-to-cart-item label').innerText
-var color = document.querySelector('.choice-2 div.options-2 label').innerText
-var flashingMessage = document.querySelector('.modal-container')
+var addToCartButtons = document.getElementsByClassName('add-to-cart');
+var title = document.querySelector('#title').innerText;
+var picture = document.getElementById('item-image').src;
+var price = document.querySelector('.add-to-cart-item label').innerText;
+var color = document.querySelector('.choice-2 div.options-2 label').innerText;
 
-console.log(flashingMessage)
+// Check login status
+var flashingMessageConfirm = document.querySelector('#process-confirmed');
+var flashingMessageDenied = document.querySelector('#process-denied');
 
 for (let i = 0; i < addToCartButtons.length; i++) {
     button = addToCartButtons[i]
     button.addEventListener('click', () => {
         if (sessionStorage.getItem('User email') == null) {
-            location.href = 'https://shynkwanchi.github.io/webprogramming_team13.github.io/login.html'
+            flashingMessageDenied.style.display = "block";
         }
         else {
-            flashingMessage.style.display = "block";
-            add_items_to_cart()
+            flashingMessageConfirm.style.display = "block";
+            add_items_to_cart();
         }
     }
     )
 }
 
-flashingMessage.addEventListener('click', function(ev) {
-    flashingMessage.style.display = "none"
+// Close the flashing message
+flashingMessageConfirm.addEventListener('click', function(ev) {
+    flashingMessageConfirm.style.display = "none"
+})
+flashingMessageDenied.addEventListener('click', function(ev) {
+    flashingMessageDenied.style.display = "none"
 })
 
 function add_items_to_cart() {
--- eworld-website/javascript/order-placement.js
+++ eworld-website/javascript/order-placement.js
@@ -1,25 +1,25 @@
-// Assigment 2: JAVASCRIPT ASSIGNMENT - TASK #7: Order Placement
+// Assigment 2: JAVASCRIPT ASSIGNMENT - TASK #8: Order Placement
 
 if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+    document.addEventListener('DOMContentLoaded', ready);
 } else {
     // Loop through all the values, which are product information of a product, of the localStorage
     for (var i = 0; i < localStorage.length; i++) {
-        var productlist = JSON.parse(localStorage.getItem(localStorage.key(i)))
+        var productlist = JSON.parse(localStorage.getItem(localStorage.key(i)));
         // Loop through all the detailed product info
         for (var item in productlist) {
             // Allocate different product info into their cateogories
-            var title = productlist[item]['name']
-            var image = productlist[item]['image']
-            var price = productlist[item]['price'].split("\n")[1]
-            var version = productlist[item]['price'].split("\n")[0]
-            var color = productlist[item]['color']
-            var inCart = productlist[item]['inCart']
-            var subTotal = updateSubTotal(price, inCart)
+            var title = productlist[item]['name'];
+            var image = productlist[item]['image'];
+            var price = productlist[item]['price'].split("\n")[1];
+            var version = productlist[item]['price'].split("\n")[0];
+            var color = productlist[item]['color'];
+            var inCart = productlist[item]['inCart'];
+            var subTotal = updateSubTotal(price, inCart);
 
-            var cartTable = document.getElementById('order-table')
-            var cartRow = document.createElement('tr')
-            cartRow.classList.add('cart-items')
+            var cartTable = document.getElementById('order-table');
+            var cartRow = document.createElement('tr');
+            cartRow.classList.add('cart-items');
             var cartRowContents = `
                     <td>
                         <div class="cart-info">
@@ -34,9 +34,9 @@ if (document.readyState == 'loading') {
                         </div>
                     </td>
                     <td><input class="quantity_input" type="number" min="1" value="${inCart}"></td>
-                    <td class="sub-total">${subTotal}</td>`
-            cartRow.innerHTML = cartRowContents
-            cartTable.append(cartRow)
+                    <td class="sub-total">${subTotal}</td>`;
+            cartRow.innerHTML = cartRowContents;
+            cartTable.append(cartRow);
         }
     }
     ready()
@@ -45,68 +45,68 @@ if (document.readyState == 'loading') {
 updateCartTotal()
 
 function ready() {
-    var quantityInputs = document.getElementsByClassName('quantity_input')
+    var quantityInputs = document.getElementsByClassName('quantity_input');
     for (var i = 0; i < quantityInputs.length; i++) {
-        var input = quantityInputs[i]
-        input.addEventListener('change', quantityChanged)
+        var input = quantityInputs[i];
+        input.addEventListener('change', quantityChanged);
     }
 }
 
 function updateSubTotal(price, quantity) {
-    var price = parseFloat((price).replace('$', ''))
-    var quantity = parseFloat(quantity)
-    var subTotal = (price * quantity)
-    return '$' + subTotal
+    var price = parseFloat((price).replace('$', ''));
+    var quantity = parseFloat(quantity);
+    var subTotal = (price * quantity);
+    return '$' + subTotal;
 }
 
 function updateCartSubTotal() {
-    var priceElement = document.getElementsByClassName('price')
-    var quantityElement = document.getElementsByClassName('quantity_input')
+    var priceElement = document.getElementsByClassName('price');
+    var quantityElement = document.getElementsByClassName('quantity_input');
 
     for (var i = 0; i < priceElement.length; i++) {
-        var price = parseFloat(priceElement[i].innerText.replace('$', ''))
-        var quantity = quantityElement[i].value
-        var subTotal = price * quantity
-        subTotal = Math.round(subTotal * 100) / 100
-        document.getElementsByClassName('sub-total')[i].innerText = '$' + subTotal
+        var price = parseFloat(priceElement[i].innerText.replace('$', ''));
+        var quantity = quantityElement[i].value;
+        var subTotal = price * quantity;
+        subTotal = Math.round(subTotal * 100) / 100;
+        document.getElementsByClassName('sub-total')[i].innerText = '$' + subTotal;
     }
 }
 
 function updateCartTotal() {
-    var subTotal = 0
-    var itemsSubTotal = document.getElementsByClassName('sub-total')
+    var subTotal = 0;
+    var itemsSubTotal = document.getElementsByClassName('sub-total');
     for (var i = 0; i < itemsSubTotal.length; i++) {
-        subTotal = subTotal + parseFloat(itemsSubTotal[i].innerText.replace('$', ''))
-        subTotal = Math.round(subTotal * 100) / 100
+        subTotal = subTotal + parseFloat(itemsSubTotal[i].innerText.replace('$', ''));
+        subTotal = Math.round(subTotal * 100) / 100;
     }
-    document.getElementById('items-sub-total').innerText = '$' + subTotal
-    document.getElementById('total').innerText = '$' + subTotal
+    document.getElementById('items-sub-total').innerText = '$' + subTotal;
+    document.getElementById('total').innerText = '$' + subTotal;
 }
 
 function quantityChanged(event) {
-    updateCartSubTotal()
-    updateCartTotal()
-    addCoupon()
+    updateCartSubTotal();
+    updateCartTotal();
+    addCoupon();
 }
 
-var applyCoupon = document.getElementById('coupon')
-applyCoupon.addEventListener('change', addCoupon)
+var applyCoupon = document.getElementById('coupon');
+applyCoupon.addEventListener('change', addCoupon);
 
 function addCoupon() {
-    var coupon = document.getElementById('coupon')
-    var total = document.getElementById('items-sub-total').innerText.replace('$', '')
+    var coupon = document.getElementById('coupon');
+    var total = document.getElementById('items-sub-total').innerText.replace('$', '');
     if (coupon.value == "COSC2430-HD") {
         total = total * 80 / 100;
-        total = Math.round(total * 100) / 100
-        document.getElementById('total').innerText = '$' + total
+        total = Math.round(total * 100) / 100;
+        document.getElementById('total').innerText = '$' + total;
     }
     else if (coupon.value == "COSC2430-DI") {
         total = total * 90 / 100;
         total = Math.round(total * 100) / 100
-        document.getElementById('total').innerText = '$' + total
+        document.getElementById('total').innerText = '$' + total;
     }
     else if (coupon.value == '') {
-        document.getElementById('total').innerText = '$' + total
+        document.getElementById('total').innerText = '$' + total;
     }
-    else { alert('The coupon is invalid') }
+    else { alert('The coupon is invalid'); }
 }
