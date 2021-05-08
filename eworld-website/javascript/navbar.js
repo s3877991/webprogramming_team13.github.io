ca104dbac2abbcd4370f99f79eee1dc5d970c80c
Nguyen Quang Duy <s3877991@rmit.edu.vn>
2021-05-08 17:26:36 +0700

    Add comments.

--- eworld-website/javascript/navbar.js
+++ eworld-website/javascript/navbar.js
@@ -1,5 +1,14 @@
+// Assigment 2: JAVASCRIPT ASSIGNMENT - TASK #8: Order Placement.
+
+// Activate the order placement button.
 var orderPlacementPage = document.getElementsByClassName('your-cart');
+
+// Activate the flash box.
 var processDenied = document.querySelector('#process-denied');
+
+// Activate the login status event:
+/* If users first access to "Order Placement" button. The flash box appears and requires them to login or register to their account.
+After users successfully log in and access to that button again, they'll be redirected to Order Placement page */
 for (let i = 0; i < orderPlacementPage.length; i++) {
     button = orderPlacementPage[i];
     button.addEventListener('click', redirect);
