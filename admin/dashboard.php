ba6f43a5689b9b86299feff01819195aa3683efa
Nguyen Quang Duy <s3877991@rmit.edu.vn>
2021-05-15 23:15:58 +0700

    Dashboard page - Initial setup.

---
+++ admin/dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width intitial-scale=1">
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <h1>Dashboard</h1>
+</body>
+
+</html>
