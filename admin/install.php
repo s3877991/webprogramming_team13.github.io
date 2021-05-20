fc60324519054f06bb4a8d9313ec3ee50573d1e3
Nguyen Quang Duy <s3877991@rmit.edu.vn>
2021-05-20 23:16:48 +0700

    Implement PHP code in install.php file

--- admin/install.php
+++ admin/install.php
@@ -2,7 +2,43 @@
 
 <!-- PHP code area -->
 <?php
+// Start the sesion
+session_start();
 
+// VALIDATE INPUTS
+if (isset($_POST['install'])) {
+
+  // Set RegEx pattern
+  $username_pattern = "/^(?=.*[a-zA-Z0-9])(.{6,})$/i";
+  $password_pattern = "/^(?=.*[a-zA-Z0-9])(.{8,})$/i";
+
+  // Validate username input
+  if (empty($_POST['username'])) {
+    $username_error = "Please enter your username!";
+  } else if (!preg_match($username_pattern, $_POST['username'])) {
+    $username_error = "Your username must have at least 6 characters and include only letters and numbers!";
+  }
+
+  // Validate password inputs
+  if (empty($_POST['password'])) {
+    $password_error = "Please enter your password!";
+  } else if (!preg_match($password_pattern, $_POST['password'])) {
+    $password_error = "Your username must have at least 8 characters and include only letters and numbers!";
+  }
+
+  // Verify the password
+  if (empty($_POST['verify_password'])) {
+    $verify_password_error = "Please re-enter your password!";
+  } else if ($_POST['verify_password'] != $_POST['password']) {
+    $verify_password_error = "Please re-enter THE SAME password!";
+  }
+
+  else {
+    $success = "Installation Successful! Please close the tab and delete '<code>install.php</code>' file to activate the System.";
+    
+  }
+
+}
 ?>
 
 <!-- HTML Code Area -->
@@ -29,38 +65,64 @@
   <!-- Main section -->
   <main>
     <div class="form">
+
+      <p class="success">
+        <?php 
+        if (isset($success)) {
+          echo $success;
+        }
+        ?>
+      </P>
+
       <h1>Install System</h1>
 
-      <form method="get" action="login.php">
+      <form method="post" action="install.php">
 
         <div class="keyboard">
           <label for="username">Your username</label>
           <br>
           <input type="text" name="username" placeholder="Enter your username">
+          <br>
+          <!-- Display error message -->
+          <span class="error">
+            <?php
+            if (isset($username_error)) {
+              echo $username_error;
+            }
+            ?>
+          </span>
         </div>
 
         <div class="keyboard">
-          <label for="pass">Your password</label>
+          <label for="password">Your password</label>
           <br>
-          <input type="password" name="pass" placeholder="Enter your password">
+          <input type="password" name="password" placeholder="Enter your password">
+          <!-- Display error message -->
+          <span class="error">
+            <?php
+            if (isset($password_error)) {
+              echo $password_error;
+            }
+            ?>
+          </span>
         </div>
 
         <div class="keyboard">
-          <label for="pass">VERIFY your password</label>
+          <label for="verify_password">VERIFY your password</label>
           <br>
-          <input type="password" name="pass" placeholder="Re-enter your password">
+          <input type="password" name="verify_password" placeholder="Re-enter your password">
+          <!-- Display error message -->
+          <span class="error">
+            <?php
+            if (isset($verify_password_error)) {
+              echo $verify_password_error;
+            }
+            ?>
+          </span>
         </div>
-
-        <!--Error Message Area-->
-        <?php
-        // If users input incorrect username or password, an error message will appear
-        if (isset($status)) {
-          echo "<h3 class=\"error\">$status</h3>";
-        }
-        ?>
 
         <div class="submit">
-          <input type="submit" name="act" value="Install">
+          <input type="submit" name="install" value="Install">
         </div>
       </form>
     </div>
