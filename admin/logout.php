7edd07110b4891f7c9051197646c8ebac270defd
Nguyen Quang Duy <s3877991@rmit.edu.vn>
2021-06-01 22:01:53 +0700

    Modify Task #1 - Part #1.

--- admin/logout.php
+++ admin/logout.php
@@ -1,6 +1,11 @@
 <?php
     session_start();
 
+    // If file 'install.php' still exists, the current PHP script in this file will be terminated
+    // if (file_exists('install.php')) {
+    //    die("You have to delete <code>'install.php'</code> file manually to activate the System");
+    // } 
+
     // this page clears login session when user wants to log out
     if (isset($_SESSION['username'])) {
         unset($_SESSION['username']);
@@ -9,4 +14,3 @@
         header("location: login.php");
         exit();
     }
-?>
