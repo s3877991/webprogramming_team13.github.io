1bb5d5fc639e49f909f2391367ea480e369e6d22
phanlinh <s3881557@rmit.edu.vn>
2021-06-01 18:30:46 +0700

    Linh commit php08

--- product_functions.php
+++ product_functions.php
@@ -25,3 +25,22 @@ function get_product($product_id) {
   }
   return false;
 }
+
+function read_store_products($storeid) {
+  $file_name = 'csv_files/products.csv';
+  $fp = fopen($file_name, 'r');
+  $first = fgetcsv($fp);
+  $products = [];
+  while ($row = fgetcsv($fp)) {
+    $i = 0;
+    $product = [];
+    foreach ($first as $col_name) {
+      $product[$col_name] =  $row[$i];
+      $i++;
+    }
+    if ($product['store_id'] == $storeid) {
+      $products[] = $product;
+    }
+  }
+  return $products;
+}
